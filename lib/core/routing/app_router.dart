f65a8e2afd14ddd208d12afe873a19989c1e09a7
Karim Ahmed <devKarimAhmed@gmail.com>
2025-06-23 21:23:54 +0300

    Refactor routing to use BaseRoute for navigation and add BaseRoute implementation

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,25 +1,20 @@
 // ignore_for_file: unused_local_variable
 
+import 'package:autospa/core/routing/base_route.dart';
 import 'package:autospa/core/routing/routes.dart';
 import 'package:autospa/features/google_signin/ui/login_screen.dart';
 import 'package:autospa/features/home/ui/home_screen.dart';
 import 'package:flutter/material.dart';
 
-
 class AppRouter {
- static Route? onGenerateRoute(RouteSettings settings) {
+  static Route? onGenerateRoute(RouteSettings settings) {
     final arguments = settings.arguments;
 
     switch (settings.name) {
       case Routes.loginScreen:
-        return  MaterialPageRoute(
-          builder: (_) =>  LoginScreen(),
-        );
+        return BaseRoute(page: LoginScreen());
       case Routes.homeScreen:
-        return MaterialPageRoute(
-          builder:
-              (_) => HomeScreen(),
-        );
+        return BaseRoute(page: HomeScreen());
       default:
         return null;
     }
